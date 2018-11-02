6a27eb55a13b16ed3e8bb24cf1541c73694469cb
유라 김 <yura0525@naver.com>
2018-11-02 13:07:53 +0900

    샌드, 리시브, 커넥트 스레드를 하는중.

--- Network_Client/Sample.cpp
+++ Network_Client/Sample.cpp
@@ -1,6 +1,7 @@
 #include <winsock2.h>
 #include <iostream>
 #include "TPacket.h"
+#include "Sample.h"
 #pragma comment(lib, "ws2_32.lib")
 
 /*
@@ -35,12 +36,16 @@ void main()
 
 }*/
 
+HANDLE g_hEvent;
+
 DWORD WINAPI SendThread(LPVOID param)
 {
 	SOCKET sock = (SOCKET)param;
 	while (1)
 	{
 		char buffer[256] = { 0, };
+
+		printf("\n보낼 데이터 입력하시오?\n");
 		fgets(buffer, 256, stdin);
 
 		if (strlen(buffer) <= 1)
@@ -58,25 +63,99 @@ DWORD WINAPI SendThread(LPVOID param)
 	return 1;
 }
 
-
-bool BeginWinSock()
+DWORD WINAPI RecvThread(LPVOID arg)
 {
-	WSADATA wsa;
-	//윈속 초기화. 0이면 성공.
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-		return false;
+	SOCKET sock = (SOCKET)arg;
 
-	return true;
+	char buffer[2048] = { 0, };
+
+	int iRecvByte = 0;
+	int iStartByte = 0;
+
+	ZeroMemory(buffer, sizeof(char) * 2048);
+	
+	bool bConnect = true;
+
+	while(bConnect)
+	{
+		int iLen = 0;
+		iLen = recv(sock, (char*)&buffer[iStartByte], sizeof(char) * PACKET_HEADER_SIZE - iRecvByte, 0);
+		iRecvByte += iLen;
+
+		if (iLen == 0)
+		{
+			printf("\n--------->정상 퇴장\n");
+			break;
+		}
+		if (iLen <= SOCKET_ERROR)
+		{
+			printf("\n--------->비정상 퇴장\n");
+			break;
+		}
+
+		if (iRecvByte == PACKET_HEADER_SIZE)
+		{
+			UPACKET* pPacket = (UPACKET*)&buffer;
+			while (iRecvByte < pPacket->ph.len)
+			{
+				iLen = recv(sock, (char*)&buffer[iRecvByte], ((sizeof(char) * pPacket->ph.len) - iRecvByte), 0);
+				iRecvByte += iLen;
+
+				if (iLen == 0 || iLen <= SOCKET_ERROR)
+				{
+					printf("\niLen == 0 || iLen <= SOCKET_ERROR [연결 종료] : %s", "서버 문제로 인하여 종료되었다.\n");
+					bConnect = false;
+					break;
+				}
+			}
+
+			if (iRecvByte == pPacket->ph.len)
+			{
+				UPACKET recvMsg;
+				ZeroMemory(&recvMsg, sizeof(recvMsg));
+				memcpy(&recvMsg, pPacket, pPacket->ph.len);
+
+				switch (recvMsg.ph.type)
+				{
+					case PACKET_CHAT_MSG:
+					{
+						printf("\n[받은 메세지] : %s\n", recvMsg.msg);
+					}
+					break;
+				}
+				iStartByte = iRecvByte = 0;
+			}
+		}
+		else
+		{
+			iStartByte += iRecvByte;
+		}
+	}
+
+	shutdown(sock, SD_BOTH);
+	closesocket(sock);
+	return 1;
 }
-bool EndWinSock()
+
+DWORD WINAPI ConnectThread(LPVOID param)
 {
-	//객체 소멸
-	if (WSACleanup() == 0)
-		return false;
+	SOCKET sock = (SOCKET)param;
 
-	return true;
+	SOCKADDR_IN addr;
+	ZeroMemory(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = inet_addr("192.168.0.101");
+	addr.sin_port = htons(10000);
+
+	int iRet = connect(sock, (sockaddr*)&addr, sizeof(addr));
+	if (iRet == SOCKET_ERROR)
+	{
+		return 1;
+	}
+
+	SetEvent(g_hEvent);			//이벤트 끝나면 해줘야한다.
+	return 1;
 }
-
 int main()
 {
 	WSADATA wd;
