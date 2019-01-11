d31e60be1fe8f0628ba6c38ccb3e82870082e738
유라 김 <yura0525@naver.com>
2019-01-11 12:53:51 +0900

    ASD

--- IOCP_Server/Sample.cpp
+++ IOCP_Server/Sample.cpp
@@ -1,3 +1,4 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
@@ -9,7 +10,7 @@
 #define MAX_WORK_THREAD 4
 #define RECV 1000
 #define SEND 2000
-
+#define BUFSIZE 2047
 struct OVERLAPPED_EX : OVERLAPPED
 {
 	DWORD flag;
@@ -17,14 +18,15 @@ struct OVERLAPPED_EX : OVERLAPPED
 
 struct TUser
 {
-	SOCKET sock;
-	WSABUF buffer;
-	char buf[2048];
 	OVERLAPPED_EX ov;
+	SOCKET sock;
+	char buf[BUFSIZE+1];
+	int recvbytes;
+	int sendbytes;
+	WSABUF buffer;
 };
 
-TUser user;
-HANDLE m_hIOCP;
+//HANDLE m_hIOCP;
 HANDLE m_hWorkThread[MAX_WORK_THREAD];
 
 bool BeginWinSock()
@@ -47,39 +49,88 @@ DWORD WINAPI WorkerThread(LPVOID param)
 {
 	LPOVERLAPPED ov;
 	DWORD bytesTransfer;
-	ULONG_PTR keyValue;
+	HANDLE hcp = (HANDLE)param;
 
 	while (1)
 	{
-		bool bRet = ::GetQueuedCompletionStatus(m_hIOCP, &bytesTransfer, &keyValue, &ov, 1);
+		ULONG_PTR keyValue;
+		int retval = ::GetQueuedCompletionStatus(hcp, &bytesTransfer, (PULONG_PTR)&keyValue, &ov, INFINITE);
 
 		TUser* pUser = (TUser*)keyValue;
 		OVERLAPPED_EX* ovex = (OVERLAPPED_EX*)&ov;
 		DWORD trans = 0;
-		if (bRet == TRUE)
+
+		//비동기 입출력 결과 확인
+		if (retval == 0 || bytesTransfer == 0)
 		{
-			pUser->buffer.buf = pUser->buf;
-			pUser->buffer.len = 2048;
-			trans = bytesTransfer;
-			ovex->flag = RECV;
-
-			if (ovex->flag == RECV)
+			if (retval == 0)
 			{
-				ovex->flag = SEND;
-				int iRet = WSASend(pUser->sock, &(pUser->buffer), 1, &trans, 0, (LPOVERLAPPED)&(pUser->ov), NULL);
+				DWORD temp1, temp2;
+				WSAGetOverlappedResult(pUser->sock, &pUser->ov, &temp1, FALSE, &temp2);
+				printf("Error!!!WSAGetOverlappedResult()");
 			}
-			else
-			{
-				ovex->flag = RECV;
-				int iRet = WSARecv(pUser->sock, &(pUser->buffer), 1, &trans, 0, (LPOVERLAPPED)&(pUser->ov), NULL);
 
-				/*if (iRet == SOCKET_ERROR)
+			closesocket(pUser->sock);
+			delete pUser;
+			continue;
+		}
+
+		//접속한 클라이언트 정보 출력
+		SOCKADDR_IN clientaddr;
+		int addrlen = sizeof(clientaddr);
+		getpeername(pUser->sock, (SOCKADDR*)&clientaddr, &addrlen);
+
+		//데이터 전송량 갱신
+		if (pUser->recvbytes == 0)
+		{
+			pUser->recvbytes = bytesTransfer;
+			pUser->sendbytes = 0;
+			//받은 데이터 출력
+			pUser->buf[pUser->recvbytes] = '\0';
+			printf("\n[TCP %s: %d] %s\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port), pUser->buf);
+		}
+		else
+		{
+			pUser->sendbytes += bytesTransfer;
+		}
+
+		if (pUser->recvbytes > pUser->sendbytes)
+		{
+			//데이터 보내기
+			ZeroMemory(&pUser->ov, sizeof(pUser->ov));
+			pUser->buffer.buf = pUser->buf + pUser->sendbytes;
+			pUser->buffer.len = pUser->recvbytes - pUser->sendbytes;
+
+			DWORD sendbytes;
+			retval = WSASend(pUser->sock, &pUser->buffer, 1, &sendbytes, 0, (OVERLAPPED*)&pUser->ov, NULL);
+			if (retval == SOCKET_ERROR)
+			{
+				if (WSAGetLastError() != WSA_IO_PENDING)
 				{
-					if (WSAGetLastError() != WSA_IO_PENDING)
-					{
-						return false;
-					}
-				}*/
+					printf("ERROR!!WSASend()");
+				}
+				continue;
+			}
+		}
+		else
+		{
+			pUser->recvbytes = 0;
+
+			//데이터 받기
+			ZeroMemory(&pUser->ov, sizeof(pUser->ov));
+			pUser->buffer.buf = pUser->buf;
+			pUser->buffer.len = BUFSIZE;
+
+			DWORD recvbytes;
+			DWORD flags = 0;
+			retval = WSARecv(pUser->sock, &pUser->buffer, 1, &recvbytes, &flags, &pUser->ov, NULL);
+			if (retval == SOCKET_ERROR)
+			{
+				if (WSAGetLastError() != WSA_IO_PENDING)
+				{
+					printf("Error!!!WSARecv()");
+				}
+				continue;
 			}
 		}
 	}
@@ -89,6 +140,19 @@ int main()
 {
 	if (BeginWinSock() == false)
 		return -1;
+
+	//작업자를 고용.
+	HANDLE hcp = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
+
+	//스레드 4개를 고용.
+	DWORD id;
+	for (int i = 0; i < MAX_WORK_THREAD; i++)
+	{
+		m_hWorkThread[i] = ::CreateThread(NULL, 0, WorkerThread, hcp, 0, NULL);
+		if (m_hWorkThread[i] == NULL)
+			return 1;
+		CloseHandle(m_hWorkThread[i]);
+	}
 
 	//listen socket
 	SOCKET listenSock;
@@ -104,38 +168,42 @@ int main()
 
 	listen(listenSock, SOMAXCONN);
 
-	//작업자를 고용.
-	m_hIOCP = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
-
-	//스레드 4개를 고용.
-	DWORD id;
-	for (int i = 0; i < MAX_WORK_THREAD; i++)
-	{
-		m_hWorkThread[i] = ::CreateThread(0, 0, WorkerThread, 0, 0, &id);
-	}
-
 	SOCKADDR_IN addr;
-	int addrlen = sizeof(addr);
 
 	while (1)
 	{
+		int addrlen = sizeof(addr);
 		SOCKET sock = accept(listenSock, (SOCKADDR*)&addr, &addrlen);
+		if (sock == INVALID_SOCKET)
+		{
+			printf("ERROR!!! accept()");
+			break;
+		}
+
+		printf("\n[TCP 서버] 클라이언트 접속 : IP 주소 = %s, 포트 번호 = %d\n", inet_ntoa(addr.sin_addr), ntohs(addr.sin_port));
+
+		TUser* pUser = new TUser;
+		if (pUser == NULL)
+			break;
 
 		DWORD trans, flag = 0;
-		user.sock = sock;
-		user.buffer.buf = user.buf;
-		user.buffer.len = 2048;
-		user.ov.flag = RECV;
+		pUser->sock = sock;
+		pUser->buffer.buf = pUser->buf;
+		pUser->buffer.len = BUFSIZE;
+		ZeroMemory(&pUser->ov, sizeof(pUser->ov));
+		pUser->ov.flag = RECV;
+		pUser->recvbytes = pUser->sendbytes = 0;
+		::CreateIoCompletionPort((HANDLE)sock, hcp, (ULONG_PTR)&pUser, 0);
+		
 
-		::CreateIoCompletionPort((HANDLE)sock, m_hIOCP, (ULONG_PTR)&user, 0);
-
-		int iRet = WSARecv(sock, &user.buffer, 1, &trans, &flag, (LPOVERLAPPED)&user.ov, NULL);
+		int iRet = WSARecv(sock, &pUser->buffer, 1, &trans, &flag, (LPOVERLAPPED)&pUser->ov, NULL);
 		if (iRet == SOCKET_ERROR)
 		{
 			if (WSAGetLastError() != WSA_IO_PENDING)
 			{
 				return -1;
 			}
+			continue;
 		}
 	}
 	EndWinSock();
