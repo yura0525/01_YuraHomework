62081acc987d34695605cb924941d96a701cb7aa
yura <yura0525@naver.com>
2019-01-22 22:20:54 +0900

    asdf

--- MakePacket/Sample.cpp
+++ MakePacket/Sample.cpp
@@ -1,5 +1,56 @@
 #include "Sample.h"
 
+DWORD WINAPI ClientThread(LPVOID arg)
+{
+	SOCKET sock = (SOCKET)arg;
+	TUser* pUser = &g_allClientSocket[sock];
+
+	char buffer[256] = { 0, };
+	int recvbyte = 0;
+	int iRet = 0;
+	while (1)
+	{
+		iRet = recv(sock, &buffer[recvbyte], PACKET_HEADER_SIZE - recvbyte, 0);
+		if (CheckReturn(iRet) <= 0)
+			break;
+
+		recvbyte += iRet;
+		if (recvbyte == PACKET_HEADER_SIZE)
+		{
+			UPACKET packet;
+			ZeroMemory(&packet, sizeof(packet));
+			memcpy(&packet.ph, buffer, PACKET_HEADER_SIZE);
+			
+			int rByte = 0;
+			int iRecvByte = 0;
+			do
+			{
+				rByte = recv(sock, (char*)&packet.msg[iRecvByte], packet.ph.len - iRecvByte, 0);
+				if (CheckReturn(iRet) <= 0)
+					break;
+
+				iRecvByte += rByte;
+			} while (packet.ph.len > iRecvByte);
+
+			if (CheckReturn(iRet) <= 0)
+				break;
+
+			recvbyte = 0;
+			switch (packet.ph.type)
+			{
+			case PACKET_CHAT_MSG:
+			{
+				cout << "패킷 완성:--->" << packet.msg;
+				Broadcastting(&packet);
+			}
+			break;
+			}
+		}
+		Sleep(1);
+	}
+	DeleteUser(sock);
+	return 0;
+}
 void main()
 {
 
--- MakePacket/Sample.h
+++ MakePacket/Sample.h
@@ -1,27 +1,36 @@
+#define  _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #pragma once
 #include <winsock2.h>
 #include <iostream>
+#include <map>
 #include "TProtocol.h"
+#include "TPacket.h"
 
 #pragma comment(lib, "ws2_32.lib")
+using namespace std;
 
+CRITICAL_SECTION	g_crit;			//유저 모드
+HANDLE				g_hMutex;		//커널 모드
 struct TUser
 {
 	SOCKET sock;
 	SOCKADDR_IN clientAddr;
 	TUser() {}
-	TUser(SOCKET client, SOCKADDR_IN add)
+	TUser(SOCKET client, SOCKADDR_IN addr)
 	{
 		sock = client;
-		clientAddr = add;
+		clientAddr = addr;
 	}
 	TUser(const TUser& user)
 	{
 		sock = user.sock;
 		clientAddr = user.clientAddr;
 	}
-	int iIndex;
 };
+
+map<SOCKET, TUser> g_allClientSocket;
+typedef map<SOCKET, TUser>::iterator ITOR;
 
 int SendMsg(SOCKET sock, char* msg, WORD type)
 {
@@ -137,4 +146,90 @@ bool EndWinSock()
 	if (iRet != 0) return false;
 
 	return true;
+}
+
+int PacketSend(SOCKET sock, char* msg, WORD type)
+{
+	TPacket packet(type);
+	packet << msg;
+
+	int sendbytes = 0;
+	int iTotalsize = packet.m_uPacket.ph.len + PACKET_HEADER_SIZE;
+
+	char* pMsg = (char*)&packet.m_uPacket;
+	int iSend = 0;
+	do {
+		iSend = send(sock, &pMsg[sendbytes], iTotalsize - sendbytes, 0);
+		if (CheckReturn(iSend) <= 0)
+		{
+			return iSend;
+		}
+		sendbytes += iSend;
+	} while (sendbytes < iTotalsize);
+
+	return sendbytes;
+}
+
+int Broadcastting(UPACKET* pPacket)
+{
+	int iUserCounter = 0;
+	DWORD dwRet = WaitForSingleObject(g_hMutex, INFINITE);			//대기 함수
+	if (dwRet != WAIT_TIMEOUT)
+	{
+		for (ITOR itor = g_allClientSocket.begin(); itor != g_allClientSocket.end();)
+		{
+			TUser* pUser = (TUser*)&itor->second;
+			if (pUser != NULL)
+			{
+				int iSendByte = PacketSend(pUser->sock, (char*)pPacket, PACKET_CHAT_MSG);
+				if (CheckReturn(iSendByte) <= 0)
+				{
+					printf("\n퇴장(Send) : IP:%s, PORT:%d\n",
+						inet_ntoa(pUser->clientAddr.sin_addr), ntohs(pUser->clientAddr.sin_port));
+					g_allClientSocket.erase(itor++);
+				}
+				else
+				{
+					iUserCounter++;
+					++itor;
+				}
+			}
+		}
+	}
+	ReleaseMutex(g_hMutex);
+	return -1;
+}
+
+void DeleteUser(SOCKET sock)
+{
+	DWORD dwRet = WaitForSingleObject(g_hMutex, INFINITE);			//대기 함수
+	ITOR itor = g_allClientSocket.find(sock);
+
+	if (itor != g_allClientSocket.end())
+	{
+		TUser* pUser = (TUser*)&itor->second;
+		if (pUser != NULL)
+		{
+			printf("\n퇴장(Send) : IP:%s, PORT:%d\n",
+				inet_ntoa(pUser->clientAddr.sin_addr), ntohs(pUser->clientAddr.sin_port));
+		}
+		g_allClientSocket.erase(itor);
+	}
+
+	closesocket(sock);
+	ReleaseMutex(g_hMutex);
+}
+
+void GreetMessage(TUser& user)
+{
+	printf("\n클라이언트 접속 : IP:%s, PORT:%d\n",
+		inet_ntoa(user.clientAddr.sin_addr), ntohs(user.clientAddr.sin_port));
+
+	char message[MAX_PATH] = { 0, };
+	sprintf_s(message, "[서버]환영합니다.");
+	PacketSend(user.sock, message, PACKET_CHAT_MSG);
+
+	DWORD dwRet = WaitForSingleObject(g_hMutex, INFINITE);		// 대기함수
+	g_allClientSocket.insert(make_pair(user.sock, user));
+	ReleaseMutex(g_hMutex);
 }
